64b911fd1bb7a90b95a3195c9cfec2e8d2b9df2d
hxshandle <handle.huang0409@gmail.com>
2014-09-15 16:19:34 +0800

    ab

---
+++ js/_shop.js
@@ -0,0 +1,6 @@
+$(function(){
+  var map = new BMap.Map("allmap");
+  map.centerAndZoom(new BMap.Point(116.404, 39.915), 15);
+  map.addControl(new BMap.MapTypeControl());
+  map.setCurrentCity("北京");
+});
